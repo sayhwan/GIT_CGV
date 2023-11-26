f6fa1138627cf1040a8e194b5445579a98a130db
sayhwan <sehwan.yi06@gmail.com>
2023-11-26 12:15:24 +0900

    tttt

--- movies/views.py
+++ movies/views.py
@@ -6,7 +6,6 @@ from movies.models import *
 # Create your views here.
 class MovieList(ListView):
     model = Movie
-
     def get_context_data(self, **kwargs):
         login_session = self.request.session.get('login_session', '')
         context = super().get_context_data(**kwargs)
